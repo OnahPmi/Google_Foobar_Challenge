aba986e8e4b0ce60312693a9b00cadfebca47a2f
OnahPmi <86573589+OnahPmi@users.noreply.github.com>
2022-12-13 11:19:40 +0100

    Update Google Foobar Challenge Level 1.py

--- Level 1/Google Foobar Challenge Level 1.py
+++ Level 1/Google Foobar Challenge Level 1.py
@@ -1,70 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Link to Challenge
-# https://foobar.withgoogle.com
-
-# # Requirements
-# ## Java
-# Your code will be compiled using standard Java 8. All tests will be run by calling the solution() method inside the Solution class
-# 
-# Execution time is limited.
-# 
-# Wildcard imports and some specific classes are restricted (e.g. java.lang.ClassLoader). You will receive an error when you verify your solution if you have used a blacklisted class.
-# 
-# Third-party libraries, input/output operations, spawning threads or processes and changes to the execution environment are not allowed.
-# 
-# Your solution must be under 32000 characters in length including new lines and and other non-printing characters.
-# ***
-# ## Python
-# Your code will run inside a Python 2.7.13 sandbox. All tests will be run by calling the solution() function.
-# 
-# Standard libraries are supported except for bz2, crypt, fcntl, mmap, pwd, pyexpat, select, signal, termios, thread, time, unicodedata, zipimport, zlib.
-# 
-# Input/output operations are not allowed.
-# 
-# Your solution must be under 32000 characters in length including new lines and and other non-printing characters.
-# ***
-
-# Level one: I Love Lance & Janice
-# ===========================
-# 
-# # Challenge
-# 
-# You've caught two of your fellow minions passing coded notes back and forth -- while they're on duty, no less! Worse, you're pretty sure 
-# it's not job-related -- they're both huge fans of the space soap opera ""Lance & Janice"". You know how much Commander 
-# Lambda hates waste, so if you can prove that these minions are wasting time passing non-job-related notes, it'll put you that much closer to a 
-# promotion. 
-# 
-# Fortunately for you, the minions aren't exactly advanced cryptographers. In their code, every lowercase letter [a..z] is replaced with the 
-# corresponding one in [z..a], while every other character (including uppercase letters and punctuation) is left untouched.  That is, 'a' becomes 
-# 'z', 'b' becomes 'y', 'c' becomes 'x', etc.  For instance, the word ""vmxibkgrlm"", when 
-# decoded, would become ""encryption"".
-# 
-# Write a function called solution(s) which takes in a string and returns the deciphered string so you can show the commander proof that these minions 
-# are talking about ""Lance & Janice"" instead of doing their jobs.
-# 
-# ## Languages
-# To provide a Python solution, edit solution.py
-# To provide a Java solution, edit solution.java
-# 
-# ## Test cases
-# 
-# Inputs:  
-# (string) s = “wrw blf hvv ozhg mrtsg’h vkrhlwv?”  
-# Output:  
-# (string) “did you see last night’s episode?”  
-# 
-# Inputs:  
-# (string) s = “Yvzs! I xzm’g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!”  
-# Output:    
-# (string) “Yeah! I can’t believe Lance lost his job at the colony!!”    
-# 
-# Use verify [file] to test your solution and see how it does. When you are finished editing your code, use submit [file] to submit your answer. If your solution passes the test cases, it will be removed from your home folder.
-
-# In[1]:
-
-
 def test_cases():
 
     test_datasets = ["Lzmxv & Jzmrxv", "wrw blf hvv ozhg mrtsg'h vkrhlwv?", 
@@ -81,11 +14,7 @@ def test_cases():
             print(f"Test {count} failed!")
 
 
-# Solutuion
-# ========
-
-# In[2]:
-
+# Solutuions
 
 # This is the solution that was submitted
 
@@ -135,10 +64,6 @@ def solution(s):
 
 test_cases()
 
-
-# In[3]:
-
-
 # Method 2
 def solution(s):
     import string
@@ -159,4 +84,3 @@ def solution(s):
     return answer
 
 test_cases()
-
